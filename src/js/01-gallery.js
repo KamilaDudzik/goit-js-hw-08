72496b66b84881be67711b5526c0d48708a5d305
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-04-18 14:23:30 +0200

    Task-01 built

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.innerHTML = '';
+for (const galleryItem of galleryItems) {
+
+    const newItem = `
+    
+    <a class="gallery__item" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+      />
+      </a>`
+    
+    gallery.innerHTML += newItem;
+};
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom"
+});
 
 console.log(galleryItems);
