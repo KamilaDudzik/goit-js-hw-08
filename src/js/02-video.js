f787043f027e4367039dab5fb5ea04ad1150ae3e
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-04-18 14:24:17 +0200

    Task-02 built

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+// const videoPlayer = document.querySelector("iframe");
+// const video = new Vimeo.Player(iframe);
+
+// player.on('play', function () {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//     console.log('title:', title);
+// });
+
+const videoPlayer = document.getElementById("vimeo-player");
+const video = new Player(videoPlayer);
+
+function saveTime(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+};
+
+const saveTimeDelayed = throttle(saveTime, 1000);
+
+const saveCurrentTime = localStorage.getItem("videoplayer-current-time");
+
+video
+    .setCurrentTime(saveCurrentTime)
+    .on("timeupdate", saveTimeDelayed);
+
+// test
