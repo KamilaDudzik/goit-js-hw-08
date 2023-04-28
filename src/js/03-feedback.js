0dcdebe0614fb25fa8c62f36aa3304a254af2756
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-04-28 19:37:05 +0200

    Task-03 built

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const buttonEl = document.querySelector("button[type ='submit']");
+const emailEl = document.querySelector("input[name ='email']");
+const messageEl = document.querySelector("textarea[name ='message']");
+
+const data = {
+  email: '',
+  message: '',
+};
+
+const saveData = (event) => {
+  event.preventDefault();
+   event.target.value = data[event.target.name];
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+const rememberData = () => {
+  let objectData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (objectData) {
+        emailEl.value = objectData.email;
+        messageEl.value = objectData.message;
+    } else {
+        formEl.reset();
+    };
+};
+
+rememberData();
+
+formEl.addEventListener('input', throttle(saveData, 500));
+buttonEl.addEventListener('click', localStorage.clear());
